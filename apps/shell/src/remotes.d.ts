6dc5a1164d57c5e5daba50c9a2dd36311e08431d
Valdery Alves <valdery.jur@gmail.com>
2025-06-10 21:52:39 -0300

    fix: fixes module-federation remotes types on shell
    Signed-off-by: Valdery Alves <valdery.jur@gmail.com>

---
+++ apps/shell/src/remotes.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="@rsbuild/core/types" />
+
+declare module "courses/App" {
+  const App: React.ComponentType;
+  export default App;
+}
+
+declare module "profile/App" {
+  const App: React.ComponentType;
+  export default App;
+}
